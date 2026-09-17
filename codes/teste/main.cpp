313122aa9d71398eec7d9a080f4dcf2ed4fd57ab
Felippo Lo Bianco <felippo.dasilva@outlook.com>
2026-09-17 10:26:02 -0300

    books addition

---
+++ codes/teste/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file      main.cpp
+ * @author    Your Name (your-email@example.com)
+ * @brief     your file's description
+ * @version   0.1
+ * @date      17-09-2026
+ * @copyright 2026, your company / association / school
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+// end of file main.cpp
